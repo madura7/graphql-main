087cc7f6d87edfd8ffe84bb6f3a2d8f5fc2d15fb
Madhura <madura12a@gmail.com>
2022-01-21 12:47:51 +0530

    babelrc file

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Start your es6 scripts here
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Up and running with GraphyQL");
+});
+
+app.listen(8081, () => console.log("Running at 8081"));
